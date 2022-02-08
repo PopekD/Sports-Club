b8b8fbd207c500305ea3c4fd38fa61768561f224
PopekD <dawid.popek01@gmail.com>
2022-02-08 13:28:27 +0200

    universal header and footer

---
+++ pages/contact.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css" />
+    <title>Contact us</title>
+</head>
+<body>
+    <?php
+		include "header.php";                
+	?>
+        <div class="contactmain">
+            <div class="contactinfo">
+                <div class="CU">Contact us</div>
+                <p class="normalt1" > Tell us your problem and help us to improve the services:</p>
+                    <div class="BT">
+                        <div class="BT1">E-mail: inquiries@hsc.fi</div>
+                        <div class="BT1">Phone: +358 50 4750203</div>
+                    </div>
+                    
+                    <div class="CT">
+                        <div class="Office">
+                            <p class="normalt">Or directly at our office:</p>
+                            <p class="bold">Hämeenlinna, Puusepänkatu 5 <br>We are open Monday till Friday 8:00 - 22:00  </p>
+                        </div>
+                        <div class="SC">
+                            <p class=normalt>HSC in social media</p>
+                            <p class="SC1">Facebook:@hsc_fi <br> 
+                            TikTok:@hsc_fi 
+                        </div>
+                        <div class="SC2">
+                            <p>Instagram:@hsc_fi <br>
+                            Linkedin:@hsc_fi
+                            </p>
+                        </div>
+
+                    </div>
+
+            </div>
+            <div class="contactimage"><img src="../images/contactimage.jpg" alt="building"></div>
+        </div>
+	    <?php
+		include "footer.php";                
+		?>
+</body>
+</html>
