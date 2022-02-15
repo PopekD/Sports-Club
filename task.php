d7a2fbcba661b1d32fde1ce84cb58a2300371786
D F <fedulov.da@mail.ru>
2022-02-15 18:44:43 +0300

    Done 1st task

---
+++ task.php
@@ -0,0 +1,28 @@
+<?php
+
+
+#TASK 1
+
+$filename = 'testfile.txt';
+$newfile = fopen($filename,'r+'); // created the new file with the read and write permissions
+
+if (file_exists($filename)) {
+    echo "The file $filename exists";
+} else {
+    echo "The file $filename does not exist";
+}
+
+fopen("testfile.txt", "r+"); // opened the file in the read and write mode
+fwrite($newfile, "Team 11: Denis Fedulov, Dawid Popek, Imran Irshad"); // wrote to the file team name and team members names
+fread($newfile, filesize($filename)); // read the file
+fclose($newfile); // closed the file
+
+
+#TASK 2
+
+
+
+
+
+
+?>
