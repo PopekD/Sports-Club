d68674cbc97738df58f4ee51bb4001e89fae2a95
D F <fedulov.da@mail.ru>
2022-02-15 18:46:58 +0300

    Fixed task.php

--- task.php
+++ task.php
@@ -1,6 +1,5 @@
 <?php
 
-
 #TASK 1
 
 $filename = 'testfile.txt';
@@ -13,16 +12,10 @@ if (file_exists($filename)) {
 }
 
 fopen("testfile.txt", "r+"); // opened the file in the read and write mode
-fwrite($newfile, "Team 11: Denis Fedulov, Dawid Popek, Imran Irshad"); // wrote to the file team name and team members names
+fwrite($newfile, "Team 11, Denis Fedulov"); // wrote to the file team name and team members names
 fread($newfile, filesize($filename)); // read the file
 fclose($newfile); // closed the file
 
-
 #TASK 2
-
-
-
-
-
 
 ?>
