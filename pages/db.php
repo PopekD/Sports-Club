0e8a712c54faea8d717e2b2c8f510f78a47e8f8e
PopekD <dawid.popek01@gmail.com>
2022-02-18 19:52:19 +0200

    -

--- pages/db.php
+++ pages/db.php
@@ -12,10 +12,5 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-<<<<<<< Updated upstream
 echo "Connected successfully";
 ?> 
-=======
-// echo "Connected successfully";
-?> 
->>>>>>> Stashed changes
