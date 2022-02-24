5caebc14e2090f8556d48e59fb8b65f9adbf1477
imrancsen <97589134+imrancsen@users.noreply.github.com>
2022-02-24 21:49:53 +0200

    copied db.php from main to imran

--- pages/db.php
+++ pages/db.php
@@ -1,24 +1,4 @@
 <?php
-<<<<<<< HEAD
-$servername="db";
-$username="registration";
-$password="password";
-$dbname="registration";
-
-// creating connection
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// checking connection
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
- echo "Connected successfully";
-?>
-
-<!--- this file establishes the connection to the database on phpmyadmin --->
-=======
 $servername = "db";
 $username = "root";
 $password = "password";
@@ -33,4 +13,3 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully";
 ?> 
->>>>>>> cd8c410df0f4e13b143c7f6d2b10ff23f05e351b
