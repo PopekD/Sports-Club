7d899215128c155bac4ba1663a97251f28035498
imrancsen <97589134+imrancsen@users.noreply.github.com>
2022-03-05 15:00:22 +0200

    phpmailer added, cform can be emailed/added to db

--- pages/contactform_mail.php
+++ pages/contactform_mail.php
@@ -1,8 +1,10 @@
 <?php
 include('server.php');
 ?>
-
+<head><script src="cnform_script.js"></script> </head>
 <?php
+// phpmailer is added to send email from contact form data besides adding it to database
+use PHPMailer\PHPMailer\PHPMailer;
 if(isset($_POST))
 if(isset( $_POST['name']))
 $name = $_POST['name'];
@@ -14,24 +16,59 @@ if(isset( $_POST['message']))
 $message = $_POST['message'];
 
 
-// $content="From: $name \n Email: $email \n Message: $message";
-// $to = "inquiries.hsc@gmail.com";
-// $mailheader = "From: $email \r\n";
-// mail('inquiries.hsc@gmail.com', $subject, $content, $mailheader) or die("Error!");
-// echo "Email sent!";
+require_once "PHPMailer/PHPMailer.php";
+require_once "PHPMailer/SMTP.php";
+require_once "PHPMailer/Exception.php";
 
+$mail = new PHPMailer();
 
-$contactdata2mysql = "INSERT INTO contactus_data VALUES ('0', '$name', '$email', '$subject','$message')";
+// smptp configuration    
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "inquiries.hsc@gmail.com";
+    $mail->Password = 'passwordhsc';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    $senderemail="Sender Email: ";
+    $sendername="Sender Name: ";
+    $Sbjt="Subject: ";
+    $mail->isHTML(true);
+    //sender
+    $mail->setFrom($email, $sendername.$name);
+    //recipient
+    $mail->addAddress("inquiries.hsc@gmail.com");
+    //email content
+    
+    $mail->Subject = ("$senderemail $email ($Sbjt $subject)");
+    $mail->Body = $message;
+
+    
+    if($mail->send()){
+    $contactdata2mysql = "INSERT INTO contactus_data VALUES ('0', '$name', '$email', '$subject','$message')";
+    $output = mysqli_query($conn, $contactdata2mysql);
+    echo "<h3>Thank you for messaging us, we'll contact you soon!</h3>";
+    resetform();
+    }
+    else
+    {
+        echo $status = "failed";
+        echo $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+// $contactdata2mysql = "INSERT INTO contactus_data VALUES ('0', '$name', '$email', '$subject','$message')";
+    //query with column names
 // $contactfdata = "INSERT INTO contactus_data (id, c_name, c_email, c_subject, c_message) VALUES ('0', '$name', '$email', '$subject','$message')";
-$output = mysqli_query($conn, $contactdata2mysql);
-if($output)
-        {
-            echo "<h3>Thank you for messaging us, we'll contact you soon!</h3>";
+// $output = mysqli_query($conn, $contactdata2mysql);
+// if($output)
+//         {
+//             echo "<h3>Thank you for messaging us, we'll contact you soon!</h3>";
 
-        }
-        else
-        {
-            echo "<h3> Error!</h3>";
-        }
+//         }
+//         else
+//         {
+//             echo "<h3> Error!</h3>";
+//         }
 
 ?>
