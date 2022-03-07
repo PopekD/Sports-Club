a263517f39ef98694b5c15c0fdef71478f04546e
imrancsen <97589134+imrancsen@users.noreply.github.com>
2022-03-08 00:56:48 +0200

    separate email code for shell.hamk&lclhost-updated

--- pages/contactform_mail.php
+++ pages/contactform_mail.php
@@ -2,6 +2,7 @@
 include('server.php');
 ?>
 <?php
+// this active code is to send email and insert data into database using localhost
 // phpmailer is added to send email from contact form data besides adding it to database
 use PHPMailer\PHPMailer\PHPMailer;
 if(isset($_POST))
@@ -56,19 +57,30 @@ $mail = new PHPMailer();
         echo $response = "Something went wrong.<br> Error: <br>" . $mail->ErrorInfo;
     }
 
-// simple query without mailer function:
-// $contactdata2mysql = "INSERT INTO contactus_data VALUES ('0', '$name', '$email', '$subject','$message')";
-    //query with column names
-// $contactfdata = "INSERT INTO contactus_data (id, c_name, c_email, c_subject, c_message) VALUES ('0', '$name', '$email', '$subject','$message')";
-// $output = mysqli_query($conn, $contactdata2mysql);
-// if($output)
-//         {
-//             echo "<h3>Thank you for messaging us, we'll contact you soon!</h3>";
 
-//         }
-//         else
-//         {
-//             echo "<h3> Error!</h3>";
-//         }
+    // this commented code is being used on live server as shell.hamk for mail function though data can be inserted on both hosts:
+// if(isset($_POST))
+// if(isset( $_POST['name']))
+// $name = $_POST['name'];
+// if(isset( $_POST['email']))
+// $email = $_POST['email'];
+// if(isset( $_POST['subject']))
+// $subject = $_POST['subject'];
+// if(isset( $_POST['message']))
+// $message = $_POST['message'];
+
+// $namenemail='from: '.$name;
+//     $namenemail.=', email: '.$email;
+//     if (mail ('inquiries.hsc@gmail.com', $namenemail, 'message-body: '.$message)){
+//     $contactdata2mysql = "INSERT INTO contactus_data VALUES ('0', '$name', '$email', '$subject','$message')";
+//     $output = mysqli_query($conn, $contactdata2mysql);
+//     echo "<h3>Thank you for messaging us, we'll contact you soon!</h3>";
+        
+//     }
+//     else
+//     {
+//         echo $status2 = "Could not send email.<br> ";
+//         echo $response = "Something went wrong.<br> Error: <br>";
+    
 
 ?>
